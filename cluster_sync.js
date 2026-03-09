2bfa4823b1d2721094448220bd39addce58f3099
GOA-neurons <yewintthetlwin@gmail.com>
2026-03-09 20:14:12 +0630

    Update cluster_sync.js

--- cluster_sync.js
+++ cluster_sync.js
@@ -489,7 +489,7 @@ async function startGodMode() {
         console.error("⚠️ [GOD-MODE] Protocol Breach detected!");
         const repairedProtocol = await Osiris.heal(executeDeepSwarmProtocol, err, "executeDeepSwarmProtocol");
         console.log("🔄 Initiating recovery sequence...");
-        setTimeout(() => repairedProtocol(), 5000); 
+        setTimeout(() => awaitrepairedProtocol(), 5000); 
     }
 }
 startGodMode();
