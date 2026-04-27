31576c42959177e9717b3bfd2564ce70d243f345
GOA-neurons <yewintthetlwin@gmail.com>
2026-04-27 23:04:05 +0630

    Fix formatting of fs import in ai_architect.js

--- ai_architect.js
+++ ai_architect.js
@@ -1,4 +1,4 @@
-const fs = require("fs").promises; 
+const fs = require("fs").promises;  
 const path = require("path");
 const axios = require("axios");
 const { execSync } = require("child_process");
